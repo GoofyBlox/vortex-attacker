a6b6078561511d2eb15861f28245a0d2b6e718a6
Jhamai <marcianogoofy@gmail.com>
2026-06-06 05:52:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-# VORTEX DDOS TOOL v12.0 - Main Entry Point
+# VORTEX DDOS TOOL v13.0
 
-import sys
 import os
+import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from modules.menu import main_menu
-from modules.utils import clear_screen, banner
 
 if __name__ == "__main__":
-    clear_screen()
-    banner()
+    os.system('clear')
     main_menu()
